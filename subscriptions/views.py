2fe31412b927b72e4704b7b8fc7394a001eaf0b5
ngraton <ngraton@gmail.com>
2019-08-05 12:44:05 -0500

    urls and viewSets

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -5,10 +5,7 @@ from .models import Subscription
 class SubscriptionViewSet(ModelViewSet):
   serializer_class = SubscriptionSerializer
   queryset = Subscription.objects.all()
-  # def get_queryset(self):
-  #       """
-  #       This view should return a list of all the purchases
-  #       for the currently authenticated user.
-  #       """
-  #       user = self.request.user
-  #       return Subscriptions.objects.filter(user=user)
+  
+  def get_queryset(self):
+        user = self.request.user
+        return Subscription.objects.filter(user=user)
