2c3b2eacbd078e36edd9b87b44d168e7e3df1ef5
ngraton <ngraton@gmail.com>
2019-08-05 09:41:36 -0500

    sub model

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Subscription(models.Model):
+  INTERVALS =  [
+    ('annual', 'Annual'),
+    # ('Semiannual', 'Semiannual')
+    ('quarterly', 'Quarterly'),
+    ('monthly', 'Monthly'),
+    # ('Weekly', 'Weekly')
+]
+
+  name = models.CharField()
+  due_date = models.DateField()
+  payment = models.DecimalField()
+  interval = models.CharField(max_length=10,
+        choices=INTERVALS)
+  user = models.ForeignKey('CustomUser', related_name='subscriptions', on_delete=models.CASCADE)
+
