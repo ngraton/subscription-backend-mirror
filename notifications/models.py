df2606ea72896cfaa338a6466caec5a8f559c6e8
ngraton <ngraton@gmail.com>
2019-08-12 10:56:51 -0500

    notification model

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+class Notification(models.Model):
+  phone_number = models.CharField(max_length=12)
+  user = models.ForeignKey(
+    CustomUser,
+    related_name='subscriptions', 
+    on_delete=models.CASCADE)
+  message = models.CharField(max_length=200)
+  time_stamp = models.DateTimeField(auto_now_add=True)
