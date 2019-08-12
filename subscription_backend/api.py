60f5a920a0002a1ab1d145ce74f568be8adb13a5
hmburd <h.m.burd@gmail.com>
2019-08-12 13:27:45 -0500

    added phone number model and can post new phonenumber for users

--- subscription_backend/api.py
+++ subscription_backend/api.py
@@ -1,6 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
 from subscriptions.views import SubscriptionViewSet
+from users.views import ProfileViewSet, CustomUserListView
 
 router = DefaultRouter()
 router.register('subscriptions', SubscriptionViewSet)
+router.register('profile', ProfileViewSet)
+router.register('users', CustomUserListView)
--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework.serializers import ModelSerializer
 
-from .models import CustomUser
+from .models import CustomUser, Profile
 
 class CustomUserSerializer(ModelSerializer):
   class Meta:
     model = CustomUser
-    fields = ('username', 'subscriptions', 'id', 'phone_number')
+    fields = ('username', 'subscriptions', 'id', 'profile')
     depth = 1
+
+class ProfileSerializer(ModelSerializer):
+  class Meta:
+    model = Profile
+    fields = ('phone_number', 'user')
